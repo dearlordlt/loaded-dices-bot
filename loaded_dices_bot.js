d69e05f6574eeb9cd7a951dff5ccf685d798cb7c
ssliteris <ssliteris@exadel.com>
2020-03-24 23:48:22 +0200

    feat/bot

---
+++ loaded_dices_bot.js
@@ -0,0 +1,131 @@
+const discord = require('discord.js');
+const client = new discord.Client();
+const parser = require('discord-command-parser');
+require('dotenv').config();
+const prefix = '!';
+
+client.on('ready', () => {
+    console.log(`Connected as ${client.user.tag}`);
+    client.user.setActivity('Loading dices');
+});
+
+client.on('message', msg => {
+    const parsed = parser.parse(msg, prefix);
+    if (msg.author.bot) return;
+    if (!parsed.success) return;
+
+    if (parsed.command === 'c') {
+        let dices = parseInt(parsed.arguments[0]) || 3;
+        let hasBonus = false;
+        if (parsed.arguments[0] && parsed.arguments[0].charAt(0) === '+') {
+            dices = 3;
+            hasBonus = parseInt(parsed.arguments[0].replace('+', ''));
+        }
+        if (dices > 0) {
+            let sum = 0;
+            let reply = '';
+            for (let i = 0; i < dices; i++) {
+                const roll = r();
+                sum += roll;
+                reply += roll + ',';
+            }
+            if (parsed.arguments[1] === '+') {
+                sum = sum + parseInt(parsed.arguments[2]);
+            }
+            if (hasBonus) {
+                sum = sum + hasBonus;
+            }
+            msg.reply(`roll: (${reply}) = ${sum}`);
+        } else {
+            msg.reply('how many?');
+        }
+    }
+
+    if (parsed.command === 'd') {
+        const dices = parseInt(parsed.arguments[0]);
+
+        if (!parsed.arguments[1]) {
+            msg.reply('choose effects, a.e. !d 3 BBT');
+            return;
+        }
+
+        const damage = parsed.arguments[1].split('');
+        const arr6 = Array('-', '-', '-', '-', '-', '-');
+
+        if (dices > 0) {
+            for (let i = 5; i >= 0; i--) {
+                if (damage) arr6[i] = damage.pop();
+            }
+            let reply = '';
+            let effects = '';
+            console.log(arr6);
+            for (let i = 0; i < dices; i++) {
+                const roll = r();
+                effects += arr6[roll - 1] ? arr6[roll - 1] : '';
+                reply += roll + ',';
+            }
+            msg.reply(`roll ${dices}d and damage ${parsed.arguments[1]}: Result (${effects}), Roll (${reply})`);
+        } else {
+            msg.reply('how many?');
+        }
+    }
+
+    if (parsed.command === 's') {
+        const dices = parseInt(parsed.arguments[0]);
+        const effectiveness = parseInt(parsed.arguments[1]) || 4;
+
+        if (dices > 0) {
+            let reply = '';
+            let successes = 0;
+            let rerolls = 0;
+            let botch = false;
+            for (let i = 0; i < dices; i++) {
+                const roll = r();
+                if (roll >= effectiveness) {
+                    successes += 1;
+                }
+                if (roll === 6) {
+                    rerolls += 1;
+                }
+                if (roll === 1) {
+                    botch = true;
+                }
+                reply += roll + ',';
+            }
+            if (!botch) {
+                for (let i = 0; i < rerolls; i++) {
+                    const roll = r();
+                    if (roll >= effectiveness) {
+                        successes += 1;
+                    }
+                    if (roll === 6) {
+                        rerolls += 1;
+                    }
+                    reply += '+' + roll + ',';
+                }
+            }
+            msg.reply(`roll ${dices}d: (${reply}) = ${successes} with effectiveness of ${effectiveness}`);
+        } else {
+            msg.reply('how many?');
+        }
+    }
+
+    if (parsed.command === 'h') {
+        msg.reply(`
+          COMBAT:
+            !c 3 + 10 //3d6 + 10
+            !c + 10 //3d6 + 10
+            !c //3d6
+          SOCIAL:
+            !s 5 4 //5d6 when 4 and more is success
+          DAMAGE:
+            !d 3 BBC //3d when 4 is B, 5 is B and 6 is C
+        `);
+    }
+});
+
+function r() {
+    return Math.ceil(Math.random() * 6)
+}
+
+client.login(process.env.API_KEY);
