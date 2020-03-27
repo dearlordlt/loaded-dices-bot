f300441ddd9912dc54b7b49ed372dfba909a7157
Dovydas Jarukaitis <dovjar@gmail.com>
2020-03-27 23:05:54 +0200

    fix

--- commands/variables.js
+++ commands/variables.js
@@ -49,9 +49,8 @@ class Variables {
                 let val=parseInt(args[4]);
                 
                 if(cnt===0){
-                    const name=args[1];
-                    this.localVariablesMap[author][name] =this.modValue(val,r);
-                    sendMsg(msg, `added ${args[1]}=${this.localVariablesMap[author][name]}`);
+                    this.localVariablesMap[author][args[1]] =this.modValue(val,r);
+                    sendMsg(msg, `added ${args[1]}=${this.localVariablesMap[author][args[1]]}`);
                 }else{
                     for(let idx=1;idx<=cnt;idx++){
                         const name=args[1]+idx;
