296baedf8de7251e02cb280a28f5a0880b42a259
Dovydas Jarukaitis <dovjar@gmail.com>
2020-03-27 21:46:05 +0200

    in the middle of refactor

--- loaded_dices_bot.js
+++ loaded_dices_bot.js
@@ -7,11 +7,11 @@ const { location } = require('./commands/location');
 const { spell } = require('./commands/spell');
 const { social } = require('./commands/social');
 const { damage } = require('./commands/damage');
+const { variables } = require('./commands/variables');
 require('dotenv').config();
 
 const client = new discord.Client();
 const prefix = '!';
-const localVariablesMap = {};
 let environment = new Environment();
 client.on('ready', () => {
     console.log(`Connected as ${client.user.tag}`);
@@ -29,7 +29,7 @@ client.on('message', msg => {
     if (!parsed.success) return;
 
     if (parsed.command === 'var') {
-        varCommandHandler(msg);
+        variables.handle(msg);
         return;
     }
 
@@ -84,7 +84,7 @@ client.on('message', msg => {
             !s 3 //3d6 when 4 and more is success
           **DAMAGE:**
             !d 3 BBC //3d when 4 is B, 5 is B and 6 is C
-          ${printVarHelp()}
+          ${variables.help()}
           **SPELL:**
             !spell //rolls 3d spell roll
           **LOCATIONS**
@@ -107,44 +107,7 @@ client.on('message', msg => {
         `);
     }
 });
-const combatCommandHandler = (msg) => {
-    let args = msg.content.match(/!c\s*(\d*)*\s*([a-z]*)*\s*([+-])*\s*(\d*)*/i);
-    let dices = parseInt(args[1] || "3");
-    const variable = args[2] || "<not exists>";
-    let sign = (args[3] === "-") ? -1 : 1;
-    let mod = parseInt(args[4] || "0");
-    mod = sign * mod;
 
-    if (dices > 0) {
-        const bonus = getVariable(msg.author.id, variable);
-        if (bonus !== 0) {
-            sendMsg(msg, `using ${variable}=${bonus}`);
-            mod = mod + bonus;
-        }
-        let roll = [...Array(dices)].map(el => el = r());
-        let initialRollSum = roll.reduce((a, b) => a + b, 0);
-        let line = `roll: [${decorateRoll(roll, dices)}] = ${initialRollSum}`;
-        if (initialRollSum > 16)
-            line = `${line} **critical success !!!** `;
-        if (initialRollSum < 5)
-            line = `${line} **critical failure !!!** `;
-
-        if (initialRollSum <= environment.autofail) {
-            line = `${line} **autofail**`;
-            sendMsg(msg, line);
-            return;
-        }
-
-        if (mod != 0)
-            line = `${line} ${(mod > 0) ? '+' + mod : mod}=${initialRollSum + mod}`;
-
-        sendMsg(msg, line);
-
-
-    } else {
-        sendMsg(msg, 'how many?');
-    }
-}
 const environmentCommandHandler = (msg) => {
     let args = msg.content.match(/!env\s*((autofail\s(\d+))*(clear)*(list)*)\s*/i);
     if (args) {
@@ -168,59 +131,7 @@ const environmentCommandHandler = (msg) => {
 
 }
 
-const varCommandHandler = (msg) => {
-    const author = msg.author.id;
 
-    if (!(author in localVariablesMap))
-        localVariablesMap[author] = {};
-    let args = msg.content.match(/!var\s*((list)*(clear)*)\s*/i);
-    if (args) {
-        if (args[1] === 'list') {
-            let lines = '';
-            const myVars = localVariablesMap[author];
-            Object.keys(myVars).forEach(e => lines = `${lines}\n ${e}=${myVars[e]}`);
-            msg.reply(`**Variables:**\n${lines}`);
-            return;
-        }
-        else if (args[1] === 'clear') {
-            delete localVariablesMap[author];
-            sendMsg(msg, `all variables cleared`);
-            return;
-        }
-
-    }
-    args = msg.content.match(/!var\s*([a-z]+)\s*(\S*)/i);
-    if (args) {
-        if (args[2]) {
-            localVariablesMap[author][args[1]] = parseInt(args[2]);
-            sendMsg(msg, `added ${args[1]}=${args[2]}`);
-        }
-        else if (args[1] in localVariablesMap[author]) {
-            delete localVariablesMap[author][args[1]];
-            sendMsg(msg, `removed ${args[1]}`);
-        }
-        return;
-    }
-    //print help
-    sendMsg(msg, printVarHelp());
-
-}
-
-const printVarHelp = () => {
-    return `**VAR:**
-            !var bow 18 //sets bow to 18 for user
-            !c bow //rolls 3d + bow
-            !var list //list all variables for user
-            !var clear //clear all variable for user
-            !var bow //removes only bow variable`;
-}
-
-const getVariable = (author, varName) => {
-    if (author in localVariablesMap)
-        if (varName in localVariablesMap[author])
-            return localVariablesMap[author][varName];
-    return 0;
-}
 
 
 client.login(process.env.API_KEY);
