4b1c89fd8c23095274763baefbef713564e55b2f
ssliteris <ssliteris@exadel.com>
2020-03-28 21:28:59 +0200

    feat/template

--- routes/characters.js
+++ routes/characters.js
@@ -2,12 +2,26 @@ const express = require('express');
 const ejs = require('ejs');
 
 const tpl = `
+  <div><h1>Attributes</h1></div>
   <div>STR: <%= character.attr.str %></div>
   <div>STR: <%= character.attr.sta %></div>
   <div>STR: <%= character.attr.ref %></div>
   <div>STR: <%= character.attr.dex %></div>
   <div>STR: <%= character.attr.per %></div>
   <div>STR: <%= character.attr.will %></div>
+
+  <div><h1>Combat Skills</h1></div>
+  <ul class="skills">
+    <% for (var i = 0; i < character.attr.combatSkills[i]..length; i++) { %>
+      <li class="skill">
+          <div>STR: <%= character.attr.combatSkills[i].name %></div>
+          <div>STR: <%= character.attr.combatSkills[i].lvl %></div>
+          <div>STR: <%= character.attr.combatSkills[i].defense %></div>
+          <div>STR: <%= character.attr.combatSkills[i].attack %></div>
+      </li>
+    <% } %>
+</ul>
+
 `;
 
 const { PlayerModel } = require('../models/player');
