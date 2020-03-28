118605eb7a0d1c5abb7951ad10b7b07ecb951068
ssliteris <ssliteris@exadel.com>
2020-03-28 13:27:09 +0200

    feat/ejs

--- commands/combat.js
+++ commands/combat.js
@@ -1,27 +1,27 @@
-const { r, decorateRoll,sendMsg } = require('../utils');
+const { r, decorateRoll, sendMsg } = require('../utils');
 const { variables } = require('./variables');
 const { playerManager } = require('../playerManager');
-const combat={
-    handle(msg, environment){
+const combat = {
+    handle(msg, environment) {
         let args = msg.content.match(/!c\s*(\d*)*\s*((my.)?([a-z0-9]+))?\s*([+-])?\s*(\d*)?/i);
         let dices = parseInt(args[1] || "3");
-        let varPrefix=args[3];
+        let varPrefix = args[3];
         const variable = args[4] || "<not exists>";
         let sign = (args[5] === "-") ? -1 : 1;
         let mod = parseInt(args[6] || "0");
         mod = sign * mod;
-    
+
         if (dices > 0) {
             let bonus = 0;
-            if(varPrefix){
-                const player= playerManager.getPlayer(msg.author.id);
-                bonus =player.getCombatSkillValue(variable,'attack');
+            if (varPrefix) {
+                const player = playerManager.getPlayer(msg.author.id);
+                bonus = player.getCombatSkillValue(variable, 'attack');
                 if (bonus !== 0) {
-                    sendMsg(msg, `using my ${variable}=${player.getCombatSkillDescription(variable,'attack')}`);
+                    sendMsg(msg, `using my ${variable}=${player.getCombatSkillDescription(variable, 'attack')}`);
                     mod = mod + bonus;
                 }
             }
-            else{
+            else {
                 bonus = variables.getVariable(msg.author.id, variable);
                 if (bonus !== 0) {
                     sendMsg(msg, `using ${variable}=${bonus}`);
@@ -29,7 +29,7 @@ const combat={
                 }
             }
 
-            
+
             let roll = [...Array(dices)].map(el => el = r());
             let initialRollSum = roll.reduce((a, b) => a + b, 0);
             let line = `roll: [${decorateRoll(roll, dices)}] = ${initialRollSum}`;
@@ -37,28 +37,28 @@ const combat={
                 line = `${line} **critical success !!!** `;
             if (initialRollSum < 5)
                 line = `${line} **critical failure !!!** `;
-    
+
             if (initialRollSum <= environment.autofail) {
                 line = `${line} **autofail**`;
                 sendMsg(msg, line);
                 return;
             }
-    
+
             if (mod != 0)
                 line = `${line} ${(mod > 0) ? '+' + mod : mod}=${initialRollSum + mod}`;
-    
+
             sendMsg(msg, line);
-    
-    
+
+
         } else {
             sendMsg(msg, 'how many?');
         }
     },
-    help(){
+    help() {
         return `**COMBAT:**
-        !c 3 + 10 //3d6 + 10
-        !c + 10 //3d6 + 10
-        !c //3d6`;
+            !c 3 + 10 //3d6 + 10
+            !c + 10 //3d6 + 10
+            !c //3d6`;
     }
 }
 module.exports = {
