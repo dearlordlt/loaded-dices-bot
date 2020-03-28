9fb0aced3de782e019d8b170f6b6e9a88c1195ce
ssliteris <ssliteris@exadel.com>
2020-03-28 19:21:52 +0200

    lint

--- commands/player.js
+++ commands/player.js
@@ -4,37 +4,37 @@ const { sendMsg } = require('../utils');
 const mongoose = require('mongoose');
 
 class Player {
-    constructor(name) {
-        this.name = name;
-        //this.fileName = `./localDb/chars/${this.name}.json`;
+  constructor(name) {
+    this.name = name;
+    //this.fileName = `./localDb/chars/${this.name}.json`;
 
-        this.attr = {
-            str: 10,
-            sta: 10,
-            dex: 10,
-            ref: 10,
-            per: 10,
-            will: 10,
-        };
-        this.combatSkills = {
+    this.attr = {
+      str: 10,
+      sta: 10,
+      dex: 10,
+      ref: 10,
+      per: 10,
+      will: 10,
+    };
+    this.combatSkills = {
 
-        };
+    };
 
-        /*if (fs.existsSync(this.fileName)) {
-          const info = JSON.parse(fs.readFileSync(this.fileName));
-          this.attr = info.attr;
-          this.combatSkills = info.combatSkills;
-        }*/
-    }
+    /*if (fs.existsSync(this.fileName)) {
+      const info = JSON.parse(fs.readFileSync(this.fileName));
+      this.attr = info.attr;
+      this.combatSkills = info.combatSkills;
+    }*/
+  }
 
-    print() {
-        return `
+  print() {
+    return `
       ${this.printAttr().trim()}
       ${this.printCombatSkills().trim()}`;
-    }
+  }
 
-    printAttr() {
-        return `
+  printAttr() {
+    return `
         **ATTRIBUTES**
             str:${this.attr.str}
             sta:${this.attr.sta}
@@ -42,21 +42,21 @@ class Player {
             ref:${this.attr.ref}
             per:${this.attr.per}
             will:${this.attr.will}`;
-    }
+  }
 
-    printCombatSkills() {
-        let lines = '**COMBAT SKILLS**\n';
-        Object.keys(this.combatSkills).forEach((skill) => lines = `${lines}\n ${skill}=${this.combatSkills[skill].lvl} attack=${this.combatSkills[skill].attack} defense=${this.combatSkills[skill].defense}`);
-        return lines;
-    }
+  printCombatSkills() {
+    let lines = '**COMBAT SKILLS**\n';
+    Object.keys(this.combatSkills).forEach((skill) => lines = `${lines}\n ${skill}=${this.combatSkills[skill].lvl} attack=${this.combatSkills[skill].attack} defense=${this.combatSkills[skill].defense}`);
+    return lines;
+  }
 
-    setAttr(name, value) {
-        this.attr[name] = value;
-    }
+  setAttr(name, value) {
+    this.attr[name] = value;
+  }
 
-    // eslint-disable-next-line class-methods-use-this
-    help() {
-        return `
+  // eslint-disable-next-line class-methods-use-this
+  help() {
+    return `
         **PLAYER**
             !p print //prints player info
             !p download //downloads player info as json file
@@ -66,189 +66,189 @@ class Player {
             !p c[ombat] bow 3 [a=ref] [d=dex]//sets bow skill to lvl 3 default attack attribute to ref and defense attribute to dex
             !p c[ombat] bow 3 a=per //set bow skill to lvl 3 and attack attribute to per
             !p c[ombat] boxing 2 d=sta//set boxing skill to lvl 2 and defense attribute to sta`;
+  }
+
+  save() {
+    /*fs.writeFileSync(this.fileName, JSON.stringify({
+      attr: this.attr,
+      combatSkills: this.combatSkills,
+    }));*/
+    playerModel.create({
+      name: this.name,
+      attr: {
+        str: this.attr.str,
+        sta: this.attr.sta,
+        dex: this.attr.dex,
+        ref: this.attr.ref,
+        per: this.attr.per,
+        will: this.attr.will,
+      },
+      combatSkills: {}
+    }, function (err, small) {
+      // saved!
+    });
+  }
+
+  handleDownloadFile(msg) {
+    const buffer = fs.readFileSync(this.fileName);
+    /**
+     * Create the attachment using MessageAttachment,
+     * overwritting the default file name to 'memes.txt'
+     * Read more about it over at
+     * http://discord.js.org/#/docs/main/master/class/MessageAttachment
+     */
+    const attachment = new MessageAttachment(buffer, `${this.name}.json`);
+    msg.channel.send(`${msg.author}, your char file!`, attachment)
+      .then((m) => {
+        m.delete(10000);
+      });
+  }
+
+  handle(msg) {
+    if ((this.handleAttr(msg) ||
+      this.handleCombatSkills(msg) ||
+      this.handleSubcommands(msg)
+    ) === false) {
+      sendMsg(msg, this.help());
     }
 
-    save() {
-        /*fs.writeFileSync(this.fileName, JSON.stringify({
-          attr: this.attr,
-          combatSkills: this.combatSkills,
-        }));*/
-        playerModel.create({
-            name: this.name,
-            attr: {
-                str: this.attr.str,
-                sta: this.attr.sta,
-                dex: this.attr.dex,
-                ref: this.attr.ref,
-                per: this.attr.per,
-                will: this.attr.will,
-            },
-            combatSkills: {}
-        }, function(err, small) {
-            // saved!
-        });
+  }
+
+  handleSubcommands(msg) {
+    const args = msg.content.match(/!p\s+(print|save)\s*/i);
+    if (!args)
+      return false;
+    switch (args[1]) {
+      case 'print':
+        sendMsg(msg, this.print());
+        break;
+      case 'save':
+        this.handleSave(msg);
+        break;
     }
 
-    handleDownloadFile(msg) {
-        const buffer = fs.readFileSync(this.fileName);
-        /**
-         * Create the attachment using MessageAttachment,
-         * overwritting the default file name to 'memes.txt'
-         * Read more about it over at
-         * http://discord.js.org/#/docs/main/master/class/MessageAttachment
-         */
-        const attachment = new MessageAttachment(buffer, `${this.name}.json`);
-        msg.channel.send(`${msg.author}, your char file!`, attachment)
-            .then((m) => {
-                m.delete(10000);
-            });
+    return true;
+  }
+
+  handleAttr(msg) {
+    const args = msg.content.match(/!p\s*(str|sta|dex|ref|per|will)\s*(\d*)/i);
+    if (!args)
+      return false;
+    if (args[1] && args[2]) {
+      this.setAttr(args[1], parseInt(args[2], 10));
+      this.save();
+      sendMsg(msg, `${args[1]}=${args[2]}`);
+    } else {
+      sendMsg(msg, this.printAttr());
     }
+    return true;
+  }
 
-    handle(msg) {
-        if ((this.handleAttr(msg) ||
-                this.handleCombatSkills(msg) ||
-                this.handleSubcommands(msg)
-            ) === false) {
-            sendMsg(msg, this.help());
-        }
-
+  getCombatSkillValue(name, actionType) {
+    const skill = this.getCombatSkill(name);
+    if (skill.lvl > 0) {
+      if (actionType in skill) return this.attr[skill[actionType]] + skill.lvl;
     }
+    return 0;
+  }
 
-    handleSubcommands(msg) {
-        const args = msg.content.match(/!p\s+(print|save)\s*/i);
-        if (!args)
-            return false;
-        switch (args[1]) {
-            case 'print':
-                sendMsg(msg, this.print());
-                break;
-            case 'save':
-                this.handleSave(msg);
-                break;
-        }
-
-        return true;
+  getCombatSkillDescription(name, actionType) {
+    const skill = this.getCombatSkill(name);
+    if (skill.lvl > 0) {
+      if (actionType in skill) return `${skill[actionType]}:${this.attr[skill[actionType]]} + ${skill.lvl}=${this.attr[skill[actionType]] + skill.lvl}`;
     }
+    return '';
+  }
 
-    handleAttr(msg) {
-        const args = msg.content.match(/!p\s*(str|sta|dex|ref|per|will)\s*(\d*)/i);
-        if (!args)
-            return false;
-        if (args[1] && args[2]) {
-            this.setAttr(args[1], parseInt(args[2], 10));
-            this.save();
-            sendMsg(msg, `${args[1]}=${args[2]}`);
-        } else {
-            sendMsg(msg, this.printAttr());
-        }
-        return true;
+  getOrCreateCombatSkill(name) {
+    if (!(name in this.combatSkills)) {
+      this.combatSkills[name] = {
+        lvl: 0,
+        attack: 'ref',
+        defense: 'dex',
+      };
     }
+    return this.combatSkills[name];
+  }
 
-    getCombatSkillValue(name, actionType) {
-        const skill = this.getCombatSkill(name);
-        if (skill.lvl > 0) {
-            if (actionType in skill) return this.attr[skill[actionType]] + skill.lvl;
-        }
-        return 0;
+  getCombatSkill(name) {
+    if ((name in this.combatSkills)) {
+      return this.combatSkills[name];
     }
+    return {
+      lvl: 0,
+      attack: 'ref',
+      defense: 'dex',
+    };
+  }
 
-    getCombatSkillDescription(name, actionType) {
-        const skill = this.getCombatSkill(name);
-        if (skill.lvl > 0) {
-            if (actionType in skill) return `${skill[actionType]}:${this.attr[skill[actionType]]} + ${skill.lvl}=${this.attr[skill[actionType]] + skill.lvl}`;
-        }
-        return '';
+  removeCombatSkill(name) {
+    if (name in this.combatSkills) {
+      delete this.combatSkills[name];
     }
+  }
 
-    getOrCreateCombatSkill(name) {
-        if (!(name in this.combatSkills)) {
-            this.combatSkills[name] = {
-                lvl: 0,
-                attack: 'ref',
-                defense: 'dex',
-            };
-        }
-        return this.combatSkills[name];
+  handleCombatSkills(msg, args) {
+    const args = msg.content.match(/!p\s+(combat|c)(\s+(add|remove)*\s*(\S+)\s*((\d+)\s*(a=(\S+))*\s*(d=(\S+))*)*)*/i);
+    if (!args)
+      return false;
+    const name = args[4];
+    const lvl = args[6];
+    const a = args[8];
+    const d = args[10];
+    const command = args[3] || 'add';
+    if (name && lvl && command === 'add') {
+      // nurodytas skill name ir lvl
+      const skill = this.getOrCreateCombatSkill(name);
+      skill.lvl = parseInt(lvl, 10);
+      if (a) skill.attack = a;
+      if (d) skill.defense = d;
+      this.save();
+      sendMsg(msg, `combat skill ${name}=${skill.lvl} attack=${skill.attack} defense=${skill.defense}`);
+    } else if (name && command === 'remove') {
+      this.removeCombatSkill(name);
+      this.save();
+      sendMsg(msg, `combat skill ${name} removed`);
+    } else if (name) {
+      const skill = this.getCombatSkill(name);
+      sendMsg(msg, `combat skill ${name}=${skill.lvl} attack=${skill.attack} defense=${skill.defense}`);
+    } else {
+      sendMsg(msg, this.printCombatSkills());
     }
-
-    getCombatSkill(name) {
-        if ((name in this.combatSkills)) {
-            return this.combatSkills[name];
-        }
-        return {
-            lvl: 0,
-            attack: 'ref',
-            defense: 'dex',
-        };
-    }
-
-    removeCombatSkill(name) {
-        if (name in this.combatSkills) {
-            delete this.combatSkills[name];
-        }
-    }
-
-    handleCombatSkills(msg, args) {
-        const args = msg.content.match(/!p\s+(combat|c)(\s+(add|remove)*\s*(\S+)\s*((\d+)\s*(a=(\S+))*\s*(d=(\S+))*)*)*/i);
-        if (!args)
-            return false;
-        const name = args[4];
-        const lvl = args[6];
-        const a = args[8];
-        const d = args[10];
-        const command = args[3] || 'add';
-        if (name && lvl && command === 'add') {
-            // nurodytas skill name ir lvl
-            const skill = this.getOrCreateCombatSkill(name);
-            skill.lvl = parseInt(lvl, 10);
-            if (a) skill.attack = a;
-            if (d) skill.defense = d;
-            this.save();
-            sendMsg(msg, `combat skill ${name}=${skill.lvl} attack=${skill.attack} defense=${skill.defense}`);
-        } else if (name && command === 'remove') {
-            this.removeCombatSkill(name);
-            this.save();
-            sendMsg(msg, `combat skill ${name} removed`);
-        } else if (name) {
-            const skill = this.getCombatSkill(name);
-            sendMsg(msg, `combat skill ${name}=${skill.lvl} attack=${skill.attack} defense=${skill.defense}`);
-        } else {
-            sendMsg(msg, this.printCombatSkills());
-        }
-        return true;
-    }
+    return true;
+  }
 }
 
 class PlayerManager {
-    constructor() {
-        this.localPlayers = {};
+  constructor() {
+    this.localPlayers = {};
+  }
+
+  getPlayer(id, name) {
+    if (!(id in this.localPlayers)) {
+      this.localPlayers[id] = new Player(name);
     }
 
-    getPlayer(id, name) {
-        if (!(id in this.localPlayers)) {
-            this.localPlayers[id] = new Player(name);
-        }
-
-        return this.localPlayers[id];
-    }
+    return this.localPlayers[id];
+  }
 }
 const playerModel = mongoose.model("Player", {
-    name: String,
-    attr: {
-        str: Number,
-        sta: Number,
-        dex: Number,
-        ref: Number,
-        per: Number,
-        will: Number,
-    },
-    combatSkills: {
+  name: String,
+  attr: {
+    str: Number,
+    sta: Number,
+    dex: Number,
+    ref: Number,
+    per: Number,
+    will: Number,
+  },
+  combatSkills: {
 
-    }
+  }
 });
 
 module.exports = {
-    Player,
-    playerManager: new PlayerManager(),
-    playerModel
+  Player,
+  playerManager: new PlayerManager(),
+  playerModel
 };
