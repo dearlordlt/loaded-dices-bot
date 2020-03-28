09d7e766d7f31a3b49b2c3ebf08e0a0e51390534
ssliteris <ssliteris@exadel.com>
2020-03-28 21:06:10 +0200

    feat/character route from mongodb

--- routes/characters.js
+++ routes/characters.js
@@ -1,22 +1,17 @@
 const express = require('express');
-const fs = require('fs');
+
+const { PlayerModel } = require('../models/player');
 
 const router = express.Router();
 
-router.get('/:name', (req, res) => {
-  const path = `./localDb/chars/${req.params.name}.json`;
-  try {
-    // eslint-disable-next-line no-console
-    console.log(fs.existsSync(path), path);
-    if (fs.existsSync(path)) {
-      const data = fs.readFileSync(path);
-      res.status(200).json(JSON.parse(data));
-    } else {
-      res.status(404).json({ error: `there is no character named ${req.params.name}` });
+router.get('/:id', (req, res) => {
+  PlayerModel.findOne({ playerId: req.params.id }, (err, character) => {
+    if (err) {
+      res.status(500).json(err);
     }
-  } catch (err) {
-    res.status(500).json(err);
-  }
+
+    res.status(200).json(JSON.parse(character));
+  });
 });
 
 module.exports = router;
