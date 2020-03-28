f21a40f484278251ec41396936d3333a907f926e
ssliteris <ssliteris@exadel.com>
2020-03-28 17:04:30 +0200

    feat/error for characters route

--- routes/characters.js
+++ routes/characters.js
@@ -11,9 +11,11 @@ router.get('/:name', (req, res) => {
     if (fs.existsSync(path)) {
       const data = fs.readFileSync(path);
       res.status(200).json(JSON.parse(data));
+    } else {
+      res.status(404).json({ error: `there is no character named ${req.params.name}` });
     }
   } catch (err) {
-    res.status(200).json(err);
+    res.status(500).json(err);
   }
 });
 
