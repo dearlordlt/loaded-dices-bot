90701e36970576f3f90de06ada0688c7aa50adb6
ssliteris <ssliteris@exadel.com>
2020-03-28 17:02:05 +0200

    feat/log

--- routes/characters.js
+++ routes/characters.js
@@ -6,6 +6,8 @@ const router = express.Router();
 router.get('/:name', (req, res) => {
   const path = `./localDb/chars/${req.params.name}.json`;
   try {
+    // eslint-disable-next-line no-console
+    console.log(fs.existsSync(path), path);
     if (fs.existsSync(path)) {
       const data = fs.readFileSync(path);
       res.status(200).json(JSON.parse(data));
