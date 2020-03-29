869efe27ca8cb3bf10c9c0b8042b6350b73db75a
Dovydas Jarukaitis <dovjar@gmail.com>
2020-03-29 12:34:12 +0300

    merge

--- context.js
+++ context.js
@@ -1,32 +1,32 @@
-class Context{
-    constructor(){
-        this.messages=[];
-    }
-    push(msg){
-        this.messages.push(msg);
-        if (this.messages.length>3)
-            this.messages.shift();
-    }
-    pop(){
-        return this.messages.pop();
-    }
+class Context {
+  constructor() {
+    this.messages = [];
+  }
+
+  push(msg) {
+    this.messages.push(msg);
+    if (this.messages.length > 3) this.messages.shift();
+  }
+
+  pop() {
+    return this.messages.pop();
+  }
 }
 
-class ContextManager{
-    constructor(){
-        this.localUsersContext={};
-    }
-    getUserContext(id){
-        if (!(id in this.localUsersContext))
-            this.localUsersContext[id] = new Context();
+class ContextManager {
+  constructor() {
+    this.localUsersContext = {};
+  }
 
-        return this.localUsersContext[id];
-    }
+  getUserContext(id) {
+    if (!(id in this.localUsersContext)) this.localUsersContext[id] = new Context();
 
+    return this.localUsersContext[id];
+  }
 }
 
 const contextManager = new ContextManager();
 module.exports = {
-    contextManager,
-    Context
-}
+  contextManager,
+  Context,
+};
