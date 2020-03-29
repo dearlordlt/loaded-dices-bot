cb339fbced88f3e14e1f63e3be469621a50abd5e
ssliteris <ssliteris@exadel.com>
2020-03-29 13:22:22 +0300

    feat/determination difficulty

--- commands/determination.js
+++ commands/determination.js
@@ -2,17 +2,18 @@ const { r, decorateRoll } = require('../utils');
 
 const printDeterminationHelp = () => `
     **DETERMINATION**
-            !de 7 //rolls 7 determination dices;`;
+            !de 7 3 //rolls 7 determination dices vs 3 difficulty;`;
 
 const determination = (args, command, sendMsg, msg) => {
   const dices = parseInt(args[0], 10);
+  const diff = parseInt(args[1], 10);
 
-  if (dices > 0) {
+  if (dices > 0 && diff > 0) {
     const roll = [...Array(dices)].map(() => r());
 
     const successDice = roll.filter((el) => el >= 4).length;
 
-    const line = `roll ${dices}d: [${decorateRoll(roll, dices)}] = ${successDice};`;
+    const line = `roll ${dices}d: [${decorateRoll(roll, dices)}] = ${successDice}; ${(successDice >= diff) ? '**failed**' : '**succeeded**'}`;
     sendMsg(msg, line, command, args);
   } else {
     sendMsg(msg, `${printDeterminationHelp()}`, command, args);
