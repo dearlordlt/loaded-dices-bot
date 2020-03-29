d7287f71e5db6cc21242d89aec625f7b8b95270a
ssliteris <ssliteris@exadel.com>
2020-03-29 13:53:59 +0300

    feat/container

--- routes/characters.js
+++ routes/characters.js
@@ -34,7 +34,9 @@ router.get('/:id', (req, res) => {
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     </head>
     <body>
-    ${md.render(text)}
+      <div class="container">
+        ${md.render(text)}
+      </div>
     </body>
   </html>`;
 
