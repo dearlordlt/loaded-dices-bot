c1c4c2b44656f6793e2080dd6ebfff62f64d75e3
ssliteris <ssliteris@exadel.com>
2020-03-30 13:20:01 +0300

    feat/social luck work in progress

--- commands/social.js
+++ commands/social.js
@@ -3,41 +3,49 @@ const { r, explode, decorateRoll } = require('../utils');
 const rolls = [];
 
 const social = (args, command, sendMsg, msg, rerollDice = 0) => {
-  const dices = parseInt(args[0], 10);
+  let dices = parseInt(args[0], 10);
   const effectiveness = parseInt(args[1], 10) || 4;
 
   if (dices > 0) {
     let roll = [];
+    let botch = false;
     if (!rerollDice) {
       roll = [...Array(dices)].map(() => r());
     } else {
-      if (!rolls) {
+      const rollsByUser = rolls.filter((el) => el.id === msg.author.id);
+      if (!rollsByUser.length) {
         sendMsg(msg, 'no recent rolls to reroll', command, args);
         return;
       }
-      roll = rolls.find((el) => el === msg.author.id).pop().roll;
+      roll = rollsByUser.pop().roll; // get last roll from user rolls
+      dices = roll.length;
       const newDice = [];
       for (let i = 0; i < rerollDice; i++) {
         newDice.push(r());
         const min = Math.min(...roll);
-        roll.filter((el) => el === min);
+        const index = roll.indexOf(min);
+        if (index > -1) {
+          roll.splice(index, 1);
+        }
       }
       roll = [...roll, newDice];
     }
 
     if (!roll.some((el) => el === 1)) {
       roll = [...roll, ...explode(roll)];
+    } else {
+      const botchNum = roll.filter((el) => el === 1).length;
+      const successNum = roll.filter((el) => el >= effectiveness).length;
+      if (botchNum >= successNum) {
+        botch = true;
+      }
     }
 
     rolls.push({ id: msg.author.id, roll, effectiveness });
 
-    // eslint-disable-next-line no-console
-    console.dir(rolls);
-
-    const botchDice = roll.filter((el) => el === 1).length;
     const successDice = roll.filter((el) => el >= effectiveness).length;
 
-    const message = (botchDice >= dices / 2 && roll.some((el) => el === 1)) ? '**botch**' : `success ${successDice >= dices ? '***skill increase!***' : ''} ${rerollDice ? 'This is luck roll' : ''}`;
+    const message = (botch) ? '**botch**' : `success ${successDice >= dices ? '***skill increase!***' : ''} ${rerollDice ? 'This is luck roll' : ''}`;
 
     const line = `roll ${dices}d: [${decorateRoll(roll, dices, effectiveness)}] = ${successDice}; ${message} with effectiveness of ${effectiveness}`;
     sendMsg(msg, line, command, args);
@@ -45,10 +53,16 @@ const social = (args, command, sendMsg, msg, rerollDice = 0) => {
       // eslint-disable-next-line no-useless-return
       return;
     }
-  } else if (args[0] === 'luck' && parseInt(args[1], 10) > 0) {
-    social(args, command, sendMsg, msg, args[0]);
+  } else if (args[0] === 'luck' && parseInt(args[1], 10) > 0 && !rerollDice) {
+    social(
+      [parseInt(args[1], 10), rolls[rolls.length.effectiveness]],
+      command,
+      sendMsg,
+      msg,
+      args[1],
+    );
   } else if (args[0] === 'log') {
-    sendMsg(msg, `your recent rolls: ${rolls}`, command, args);
+    sendMsg(msg, `your recent rolls: ${JSON.stringify(rolls)}`, command, args);
   } else {
     sendMsg(msg, 'how many?', command, args);
   }
