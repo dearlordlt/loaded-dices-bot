ab12590e55fbbd5ea042e10729c0d93f6b5aa006
ssliteris <ssliteris@exadel.com>
2020-03-30 12:29:13 +0300

    feat/social luck lol

--- commands/social.js
+++ commands/social.js
@@ -2,18 +2,30 @@ const { r, explode, decorateRoll } = require('../utils');
 
 const rolls = [];
 
-const social = (args, command, sendMsg, msg) => {
+const social = (args, command, sendMsg, msg, rerollDice = 0) => {
   const dices = parseInt(args[0], 10);
   const effectiveness = parseInt(args[1], 10) || 4;
 
   if (dices > 0) {
-    let roll = [...Array(dices)].map(() => r());
+    let roll = [];
+    if (!rerollDice) {
+      roll = [...Array(dices)].map(() => r());
+    } else {
+      roll = rolls.find((el) => el === msg.author.id).pop().roll;
+      const newDice = [];
+      for (let i = 0; i < rerollDice; i++) {
+        newDice.push(r());
+        const min = Math.min(...roll);
+        roll.filter((el) => el === min);
+      }
+      roll = [...roll, newDice];
+    }
 
     if (!roll.some((el) => el === 1)) {
       roll = [...roll, ...explode(roll)];
     }
 
-    rolls.push({ id: msg.author.id, roll });
+    rolls.push({ id: msg.author.id, roll, effectiveness });
 
     // eslint-disable-next-line no-console
     console.dir(rolls);
@@ -25,6 +37,8 @@ const social = (args, command, sendMsg, msg) => {
 
     const line = `roll ${dices}d: [${decorateRoll(roll, dices, effectiveness)}] = ${successDice}; ${message} with effectiveness of ${effectiveness}`;
     sendMsg(msg, line, command, args);
+  } else if (args[0] === 'luck' && parseInt(args[1], 10) > 0) {
+    social(args, command, sendMsg, msg, args[0]);
   } else {
     sendMsg(msg, 'how many?', command, args);
   }
@@ -33,7 +47,8 @@ const social = (args, command, sendMsg, msg) => {
 const printSocialHelp = () => `
     **SOCIAL:**
             !s 5 4 //5d6 when 4 and more is success
-            !s 3 //3d6 when 4 and more is success`;
+            !s 3 //3d6 when 4 and more is success
+            !s luck 2 //rerolls 2 lowest dice in last players roll`;
 
 module.exports = {
   social,
