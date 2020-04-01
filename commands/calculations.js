eb41065e8ff24ad9e36abe5ac31043cdec3d6bf1
ssliteris <ssliteris@exadel.com>
2020-04-01 17:30:42 +0300

    fix/mongo query

--- commands/calculations.js
+++ commands/calculations.js
@@ -4,31 +4,27 @@ const printCalculationsHelp = () => `
     **STATS**
             !calc STA 11 //shows calculations for sta;`;
 
-const getCharInfo = async (sendMsg, msg, id) => {
-  const data = await CharacterModel.findOne({ playerId: id }, (err, characterData) => {
-    if (err) {
-      sendMsg(msg, 'err');
-    }
-    return characterData;
-  });
-  return data;
-};
-
 const calculations = (args, command, sendMsg, msg) => {
   const subCommand = args[0];
 
   if (subCommand && subCommand.toUpperCase() === 'STA' && parseInt(args[1], 10)) {
     const sta = parseInt(args[1], 10);
-    const line = `1/3 STA = ${Math.ceil((sta * 2) / 3)}, 2/3 STA = ${Math.ceil(((sta * 2) / 3) * 2)}, MAX Intoxication = ${Math.ceil(sta / 3)}`;
+    const line = `Total HP: ${sta * 2}, 1/3 HP = ${Math.ceil((sta * 2) / 3)}, 2/3 HP = ${Math.ceil(((sta * 2) / 3) * 2)}, MAX Intoxication = ${Math.ceil(sta / 3)}`;
     sendMsg(msg, line, command, args);
     return;
   }
 
   if (subCommand && subCommand.toUpperCase() === 'STA') {
-    const charData = getCharInfo(sendMsg, msg, msg.author.id);
-    const { sta } = charData.attr;
-    const line = `**${charData.name}**: 1/3 STA = ${Math.ceil((sta * 2) / 3)}, 2/3 STA = ${Math.ceil(((sta * 2) / 3) * 2)}, MAX Intoxication = ${Math.ceil(sta / 3)}`;
-    sendMsg(msg, line, command, args);
+    CharacterModel.findOne({ playerId: msg.author.id }, (err, data) => {
+      console.log(data, msg.author.id);
+      if (!data) {
+        console.log(err, data);
+        return;
+      }
+      const { sta } = data.attr;
+      const line = `**${data.name}**: Total HP: ${sta * 2},  1/3 HP = ${Math.ceil((sta * 2) / 3)}, 2/3 HP = ${Math.ceil(((sta * 2) / 3) * 2)}, MAX Intoxication = ${Math.ceil(sta / 3)}`;
+      sendMsg(msg, line, command, args);
+    });
     return;
   }
 
